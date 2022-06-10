55a70d88d1e93087f13c4d684fd7c19d7c15cde9
Doyun-Claire-Lee <doyun.lee@kakao.com>
2022-06-10 11:35:20 +0900

    Component scan

---
+++ core-element-comprehension/src/test/java/com/lecture/spring/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package com.lecture.spring.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ core-element-comprehension/src/test/java/com/lecture/spring/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package com.lecture.spring.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+}
