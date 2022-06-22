8580f3420bd687a33f243ece49a032c5e76e6715
Doyun-Claire-Lee <doyun.lee@kakao.com>
2022-06-22 23:33:26 +0900

    Front Controller Pattern V3

---
+++ spring-mvc-part-one/src/main/java/com/hello/web/frontcontroller/v3/controller/MemberListControllerV3.java
@@ -0,0 +1,24 @@
+package com.hello.web.frontcontroller.v3.controller;
+
+import com.hello.domain.member.Member;
+import com.hello.domain.member.MemberRepository;
+import com.hello.web.frontcontroller.ModelView;
+import com.hello.web.frontcontroller.v3.ControllerV3;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV3 implements ControllerV3 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        List<Member> members = memberRepository.findAll();
+
+        ModelView modelView = new ModelView("members");
+        modelView.getModel().put("members", members);
+
+        return modelView;
+    }
+}
