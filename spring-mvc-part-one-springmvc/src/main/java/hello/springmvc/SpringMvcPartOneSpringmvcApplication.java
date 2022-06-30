6a2ebc8b8d95f900d2e7ed5c1cd27f3d63256fc5
Doyun-Claire-Lee <doyun.lee@kakao.com>
2022-06-30 14:56:37 +0900

    project init

---
+++ spring-mvc-part-one-springmvc/src/main/java/hello/springmvc/SpringMvcPartOneSpringmvcApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcPartOneSpringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcPartOneSpringmvcApplication.class, args);
+    }
+
+}
