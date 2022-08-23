c9d77ff15f9d33e15aaa783329b7f1424dd33076
Doyun-Claire-Lee <doyun.lee@kakao.com>
2022-08-23 17:52:28 +0900

    http servlet session

--- spring-mvc-part-two/login/src/main/java/hello/login/web/HomeController.java
+++ spring-mvc-part-two/login/src/main/java/hello/login/web/HomeController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -42,11 +44,45 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
         // 세션관리자에 저장된 회원 정보 조회
         Member member = (Member) sessionManager.getSession(request);
 
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+
+        return "loginHome";
+    }
+
+//    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+        // 로그인하지 않은 사용자도 이 요청을 보낼 수 있기 때문에 무조건 세션을 생성하지 않도록 false로 설정
+        HttpSession session = request.getSession(false);
+
+        if (session == null) {
+            return "home";
+        }
+
+        Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+
+        return "loginHome";
+    }
+
+    /*
+     * @SessionAttribute 어노테이션은 세션을 생성하지 않음.
+     */
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member, Model model) {
         if (member == null) {
             return "home";
         }
