789cbfbdfab3d7ffd6435b6743d061d68aa7f10a
Doyun-Claire-Lee <doyun.lee@kakao.com>
2022-09-02 23:54:37 +0900

    type converter intro

---
+++ spring-mvc-part-two/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,31 @@
+package hello.typeconverter.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data"); //문자 타입으로 조회됨
+        Integer intValue = Integer.valueOf(data);       //정수형으로 형변환
+        System.out.println("intValue = " + intValue);
+
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) { //문자인 param값을 스프링이 int로 변환해줌
+        System.out.println("data = " + data);
+
+        return "ok";
+    }
+
+
+}
