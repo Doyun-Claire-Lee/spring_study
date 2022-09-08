ccfa988abf9a202e3e4413ab4a2c31e2df59b59f
Doyun-Claire-Lee <doyun.lee@kakao.com>
2022-09-08 23:06:09 +0900

    using logger

--- spring-mvc-part-two/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
+++ spring-mvc-part-two/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -16,22 +16,22 @@ public class HelloController {
     public String helloV1(HttpServletRequest request) {
         String data = request.getParameter("data"); //문자 타입으로 조회됨
         Integer intValue = Integer.valueOf(data);       //정수형으로 형변환
-        System.out.println("intValue = " + intValue);
+        log.info("intValue={}", intValue);
 
         return "ok";
     }
 
     @GetMapping("/hello-v2")
     public String helloV2(@RequestParam Integer data) { //문자인 param값을 스프링이 int로 변환해줌
-        System.out.println("data = " + data);
+        log.info("data={}", data);
 
         return "ok";
     }
 
     @GetMapping("/ip-port")
     public String ipPort(@RequestParam IpPort ipPort) {
-        System.out.println("ipPort.getIp() = " + ipPort.getIp());
-        System.out.println("ipPort.getPort() = " + ipPort.getPort());
+        log.info("ipPort.getIp()={}", ipPort.getIp());
+        log.info("ipPort.getPort()={}", ipPort.getPort());
 
         return "ok";
     }
