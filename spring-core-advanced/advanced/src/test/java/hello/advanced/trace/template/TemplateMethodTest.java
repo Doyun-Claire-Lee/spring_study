ab607ed0aa76cf5937ad25ff7a9397f2ec7324f1
Doyun-Claire-Lee <doyun.lee@kakao.com>
2023-01-30 18:18:35 +0900

    3-3. Template method pattern - example code

--- spring-core-advanced/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ spring-core-advanced/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -1,5 +1,8 @@
 package hello.advanced.trace.template;
 
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +13,6 @@ public class TemplateMethodTest {
     void templateMethod() {
         logic1();
         logic2();
-
     }
 
     private void logic1() {
@@ -34,6 +36,18 @@ public class TemplateMethodTest {
         long resultTime = endTime - startTime;
 
         log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.excute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.excute();
     }
 }
 
