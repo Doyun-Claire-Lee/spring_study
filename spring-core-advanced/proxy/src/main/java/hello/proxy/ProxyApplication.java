60b3dd28d6ea43df1fee26ad0e99a66e766e9db1
Doyun-Claire-Lee <doyun.lee@kakao.com>
2023-02-03 17:01:10 +0900

    4-3. proxy project - example code

--- spring-core-advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring-core-advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,12 +1,12 @@
 package hello.proxy;
 
+import hello.proxy.config.AppConfigV1;
 import hello.proxy.config.AppConfigV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-//@Import(AppConfigV1.class)  //스프링 빈으로 등록 - app패키지부터 스캔하도록 되어있기 때문에
-@Import(AppConfigV2.class)
+@Import({AppConfigV1.class, AppConfigV2.class})  //스프링 빈으로 등록 - app패키지부터 스캔하도록 되어있기 때문에
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
