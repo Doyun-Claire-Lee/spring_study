ccb8e94abde1208c779a8b2e4a900a6904c7a6a9
Doyun-Claire-Lee <doyun.lee@kakao.com>
2023-02-06 18:05:23 +0900

    4-13. Interface-based proxy - apply

--- spring-core-advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring-core-advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,17 +1,25 @@
 package hello.proxy;
 
-import hello.proxy.config.AppConfigV1;
-import hello.proxy.config.AppConfigV2;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({AppConfigV1.class, AppConfigV2.class})  //스프링 빈으로 등록 - app패키지부터 스캔하도록 되어있기 때문에
+//@Import({AppConfigV1.class, AppConfigV2.class})  //스프링 빈으로 등록 - app패키지부터 스캔하도록 되어있기 때문에
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProxyApplication.class, args);
+    }
+
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
     }
 
 }
