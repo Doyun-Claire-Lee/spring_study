d0dbb57f46cf57029ab2479bf7015bbb34df880c
Doyun-Claire-Lee <doyun.lee@kakao.com>
2023-02-06 15:16:48 +0900

    4-11. Decorator pattern - example code

--- spring-core-advanced/proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ spring-core-advanced/proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -3,6 +3,7 @@ package hello.proxy.pureproxy.decorator;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
 import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.TimeDecorator;
 import org.junit.jupiter.api.Test;
 
 public class DecoratorPatternTest {
@@ -15,8 +16,15 @@ public class DecoratorPatternTest {
     }
 
     @Test
-    void decorator() {
+    void decorator1() {
         DecoratorPatternClient client = new DecoratorPatternClient(new MessageDecorator(new RealComponent()));
+
+        client.execute();
+    }
+
+    @Test
+    void decorator2() {
+        DecoratorPatternClient client = new DecoratorPatternClient(new TimeDecorator(new MessageDecorator(new RealComponent())));
 
         client.execute();
     }
