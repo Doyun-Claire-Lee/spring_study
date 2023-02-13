bb5eda1dfa58ad5514ae90348ee5c49445158447
Doyun-Claire-Lee <doyun.lee@kakao.com>
2023-02-13 22:36:51 +0900

    6-6. Pointcut

--- spring-core-advanced/proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ spring-core-advanced/proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -3,11 +3,18 @@ package hello.proxy.advisor;
 import hello.proxy.common.advice.TimeAdvice;
 import hello.proxy.common.service.ServiceImpl;
 import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 
+import java.lang.reflect.Method;
+
+@Slf4j
 public class AdvisorTest {
 
     @Test
@@ -19,5 +26,57 @@ public class AdvisorTest {
 
         proxy.save();
         proxy.find();
+    }
+
+    @Test
+    @DisplayName("직접 만든 포인트컷")
+    void advisorTest2() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory factory = new ProxyFactory(target);
+        factory.addAdvisor(new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice()));
+        ServiceInterface proxy = (ServiceInterface) factory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    // 스프링이 구현해놓은 포인트컷을 주로 사용하게 되기 때문에 직접 구현할일은 거의 없음.
+    static class MyPointcut implements Pointcut {
+
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+
+    static class MyMethodMatcher implements MethodMatcher {
+
+        private String matchName = "save";
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            boolean result = matchName.equals(method.getName());
+            log.info("포인트컷 호출 method={}, targetClass={}", method.getName(), targetClass);
+            log.info("포인트컷 결과 result={}", result);
+
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            // false인 경우: 위의 matches 호출
+            // true인 경우: 아래의 matches 호출
+            return false;
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            return false;
+        }
     }
 }
